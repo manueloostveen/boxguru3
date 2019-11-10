14e8207397d787cd5643c955ffcf42d2330da7e4
Manuel Oostveen <manueloostveen@gmail.com>
2019-11-10 15:57:38 +0100

    Match test gefixt naar 100% voor var dims producten.

--- products/templatetags/field_values.py
+++ products/templatetags/field_values.py
@@ -1,5 +1,7 @@
 from django import template
+
 register = template.Library()
+
 
 @register.simple_tag
 def get_field_value(instance, field_name):
@@ -7,7 +9,7 @@ def get_field_value(instance, field_name):
     returns value of field based on field name
     """
     if hasattr(instance, field_name):
-        value =  getattr(instance, field_name)
+        value = getattr(instance, field_name)
         if value == None:
             return '-'
         else:
@@ -29,6 +31,7 @@ def get_dimension_match_width_length(instance, value):
     else:
         return '-'
 
+
 @register.simple_tag
 def get_dimensions_match(instance, search_width, search_length, search_height, search_diameter):
     object_width = getattr(instance, 'inner_dim1')
@@ -37,25 +40,25 @@ def get_dimensions_match(instance, search_width, search_length, search_height, s
     object_diameter = getattr(instance, 'diameter')
     object_min_height = getattr(instance, 'inner_variable_dimension_MIN')
     object_max_height = getattr(instance, 'inner_variable_dimension_MAX')
-
-    if object_max_height and not object_min_height:
-        object_min_height = 0
-    if object_max_height:
-        print(object_min_height,'-', object_max_height)
+    print(object_min_height)
 
     def get_match(search_dim, object_dim):
         if search_dim and object_dim:
             return round((1 - (abs((search_dim - object_dim)) / object_dim)) * 100)
         return 0
 
+    def get_var_match():
+        if object_min_height is not None and object_max_height is not None:
+            return 100
+
+
     swidth_width_match = get_match(search_width, object_width)
     swidth_length_match = get_match(search_width, object_length)
     slength_width_match = get_match(search_length, object_width)
     slength_length_match = get_match(search_length, object_length)
     sheight_height_match = get_match(search_height, object_height)
+    sheight_varheight_match = get_var_match()
     sdiameter_diameter_match = get_match(search_diameter, object_diameter)
-
-
 
     if swidth_width_match >= slength_width_match:
         match1 = swidth_width_match
@@ -67,11 +70,15 @@ def get_dimensions_match(instance, search_width, search_length, search_height, s
     else:
         match2 = swidth_length_match
 
-    match3 = sheight_height_match
+    if sheight_varheight_match:
+        match3 = sheight_varheight_match
+    else:
+        match3 = sheight_height_match
+
     match4 = sdiameter_diameter_match
 
     all_matches = [match1, match2, match3, match4]
-    found_matches =[match for match in all_matches if match > 0]
+    found_matches = [match for match in all_matches if match > 0]
     if len(found_matches) > 0:
         overall_match = sum(found_matches) / len(found_matches)
     else:
@@ -86,4 +93,3 @@ def get_dimensions_match(instance, search_width, search_length, search_height, s
         else:
             match1 = 0
         return match1, match2, match3, match4, overall_match
-
