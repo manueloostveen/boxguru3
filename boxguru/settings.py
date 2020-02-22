7402421f890678532c13e8f80e0a78a0de7628f6
Manuel Oostveen <manueloostveen@gmail.com>
2020-02-22 16:09:24 +0100

    initial commit for now heroku app textboxguru

--- boxguru/settings.py
+++ boxguru/settings.py
@@ -36,7 +36,8 @@ SECRET_KEY = ')udxer79(350b(crjy8-1c7xatq&mmjlku3^ti2v*bjj2u(m32'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.8.100', '127.0.0.1', '0.0.0.0', 'herokuboxguru.herokuapp.com']
+ALLOWED_HOSTS = ['192.168.8.100', '127.0.0.1', '0.0.0.0', 'herokuboxguru.herokuapp.com',
+                 'testboxguru.herokuapp.com']
 
 # Application definition
 
