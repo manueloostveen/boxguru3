6e2757b9dfc0f764a7f26394e3bdd169c62b00aa
Manuel Oostveen <manueloostveen@gmail.com>
2020-02-27 14:50:05 +0100

    collectstatic run locally

--- boxguru/settings.py
+++ boxguru/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -178,3 +178,5 @@ ACCOUNT_LOGIN_ATTEMPTS_LIMIT = None
 import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
