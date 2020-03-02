6059c46c3094ffd0f9f99d7f92c4817b876aeca8
Manuel Oostveen <manueloostveen@gmail.com>
2020-03-02 10:39:11 +0100

    secure_ssl_redirects=True

--- boxguru/settings.py
+++ boxguru/settings.py
@@ -194,3 +194,6 @@ prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 django_heroku.settings(locals(), logging=False)
+
+#SSL redirets
+SECURE_SSL_REDIRECTS = True
