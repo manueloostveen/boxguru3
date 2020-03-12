182b88bab4d61f5e2c62382cd5d51a0457b37b1b
Manuel Oostveen <manueloostveen@gmail.com>
2020-03-12 15:34:40 +0100

    date field added to referral object

--- analysis/models.py
+++ analysis/models.py
@@ -5,6 +5,8 @@ class Referral(models.Model):
     url = models.URLField(blank=True, null=True)
     domain = models.URLField(blank=True, null=True)
     sessionid = models.CharField(max_length=100, blank=True, null=True)
+    date = models.DateField(auto_now_add=True, null=True, blank=True)
+
 
     def __str__(self):
         return str(self.url)
